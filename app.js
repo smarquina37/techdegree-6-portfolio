4157ef261e7aacd3f4b7357a6de019d3a6ef03e7
smarquina37 <56809615+smarquina37@users.noreply.github.com>
2022-10-11 07:52:31 -0700

    Added about and home routes

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require("express");
+// const projectData = require("/data.json");
 const app = express();
 const port = 3000;
 
+//Set the absolute path in the express.static function
+app.use("/static", express.static("public"));
+
+//Set the view engine property to pug
+app.set("view engine", "pug");
+
+//Set routes
+// const homeRoute = require("/");
+// const aboutRoute = require("/about");
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 //app.listen() is the function that starts a port and host, in our case the
 //localhost for the connections to listen to incoming requests from a client.
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
---
+++ routes/about.js
@@ -0,0 +1,5 @@
+//Bring in express so you can use express router
+const express = require("express");
+const router = express.Router();
+
+module.exports = router;
